22db2343e11a5fac3608ef20da56149ad043fb7b
dgomez <didier.gomez@netlogistik.com>
2020-10-11 01:01:14 -0500

    added prometheus metrics

--- src/main/java/com/dgomez/kafka/CursoKafkaSpringApplication.java
+++ src/main/java/com/dgomez/kafka/CursoKafkaSpringApplication.java
@@ -15,22 +15,29 @@ import org.springframework.kafka.core.KafkaProducerException;
 import org.springframework.kafka.core.KafkaSendCallback;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.concurrent.ListenableFuture;
 
+import io.micrometer.core.instrument.MeterRegistry;
+
 @SpringBootApplication
-public class CursoKafkaSpringApplication implements CommandLineRunner {
+public class CursoKafkaSpringApplication {
+	public static final String DEFAULT_TOPIC = "ecommerce-topic"; 
+	public static Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
 
 	@Autowired
 	private KafkaTemplate<String, String> kafkaTemplate;
 	
 	@Autowired
-	private KafkaListenerEndpointRegistry registry;
+	private KafkaListenerEndpointRegistry listenerRegistry;
+	
+	@Autowired
+	private MeterRegistry meterRegistry;
 
-	public static Logger log = LoggerFactory.getLogger(CursoKafkaSpringApplication.class);
 
-	@KafkaListener(id="ecommerce-listener", autoStartup = "false", 
-			topics = "ecommerce-topic", containerFactory = "concurrentListener", 
-			properties = {"max.poll.interval.ms:1000", "max.poll.records:100"},
+	@KafkaListener(id="ecommerce-listener", topics = DEFAULT_TOPIC, 
+			containerFactory = "concurrentListener", 
+			properties = {"max.poll.interval.ms:4000", "max.poll.records:50"},
 			groupId = "demo-group")
 	public void listen(List<ConsumerRecord<String, String>> messages) {
 		log.info("Process batch started...");
@@ -44,40 +51,53 @@ public class CursoKafkaSpringApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CursoKafkaSpringApplication.class, args);
 	}
+	
+	@Scheduled(fixedDelay = 2000, initialDelay = 100)
+	public void scheduledMessages() {
+		sendMessagesSimple("Purcharse operation", 200, 100);
+	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		// sendInitialMsgWithCallback();
-		for (int i = 0; i < 1000; i++) {
-			kafkaTemplate.send("ecommerce-topic", String.valueOf(i/100), String.format("Purcharse operation %d", i));
+	
+
+	@Scheduled(fixedDelay = 2000, initialDelay = 500)
+	private void printMetrics() {
+		double count = meterRegistry.get("kafka.producer.record.send.total").functionCounter().count();
+		log.info("count: {}", count);
+	}
+	
+	private void sendMessagesSimple(String msg, int messageQuantity, int keyOffset) {
+		for (int i = 0; i < messageQuantity; i++) {
+			kafkaTemplate.send(DEFAULT_TOPIC, String.valueOf(i/keyOffset), String.format(msg + i));
 		}
+	}
+	private void manualStartupDemo() throws InterruptedException {
+		// Send messages first
+		for (int i = 0; i < 100; i++) {
+			kafkaTemplate.send(DEFAULT_TOPIC, String.valueOf(i/100), String.format("Purcharse operation %d", i));
+		}
+		// Delay 1 second
 		Thread.sleep(1000);
+		// Manually start listener
 		log.info("Start listener");
-		registry.getListenerContainer("ecommerce-listener").start();
+		listenerRegistry.getListenerContainer("ecommerce-listener").start();
 	}
 
 	private void sendInitialMsgWithCallback() {
-
-		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send("ecommerce-topic", "initial message");
+		ListenableFuture<SendResult<String, String>> future = kafkaTemplate.send(DEFAULT_TOPIC, "initial message");
 		future.addCallback(new KafkaSendCallback<String, String>() {
-
 			@Override
 			public void onSuccess(SendResult<String, String> result) {
 				log.info("Message sent {}", result.getRecordMetadata().offset());
 			}
-
 			@Override
 			public void onFailure(Throwable ex) {
 				log.error("Error sending message", ex.getMessage());
 			}
-
 			@Override
 			public void onFailure(KafkaProducerException ex) {
 				log.error("Error sending message", ex.getMessage());
 			}
-
 		});
-
 	}
 
 }
--- src/main/java/com/dgomez/kafka/config/KafkaConfig.java
+++ src/main/java/com/dgomez/kafka/config/KafkaConfig.java
@@ -15,11 +15,13 @@ import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.MicrometerProducerListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.prometheus.PrometheusConfig;
 import io.micrometer.prometheus.PrometheusMeterRegistry;
 
+@EnableScheduling
 @Configuration
 public class KafkaConfig {
 	public Map<String, Object> consumerProps() {
